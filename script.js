928444aced0b4306b5afa1891239cf795c9dd499
Albeng <alvinalvaro146@gmail.com>
2023-05-20 11:33:58 +0800

    new

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function PersonConstructor(name, age) {
+/*function PersonConstructor(name, age) {
   this.name = name;
   this.age = age;
   this.sayHello = function () {
@@ -32,4 +32,35 @@ const person2 = personFactory("Ruth Joy", 18);
 person2.sayHello();
 
 const person3 = new Person("luis", 13);
-person3.sayHello();
+person3.sayHello(); */
+
+class Animal {
+    constructor(name, species, age) {
+        this.name = name;
+        this.species = species;
+        this.age = age
+    }
+
+    introduce() {
+        console.log(`Hi, my name is ${this.name}. I'm a ${this.species} and I'm ${this.age} years old`);
+}
+
+birthday() {
+    this.age++;
+    }
+}
+
+const animal1 = new Animal("Fluffy", "cat", 2);
+const animal2= new Animal("Buddy", "dog", 4);
+const animal3 = new Animal("Chirpy", "bird", 1);
+
+animal1.introduce();
+animal2.introduce();
+animal3.introduce();
+
+animal2.birthday();
+console.log(`It's ${animal2.name}'s birthday!`);
+animal2.introduce();
+
+
+
